a8d3824e09b138b73b5d0cce2e4943c09f4220b7
Tomáš Hobza <hobza.tomas@gmail.com>
2024-03-27 14:20:36 +0100

    feat: :fire: tcp base methods

--- include/TcpPostman.hpp
+++ include/TcpPostman.hpp
@@ -1,21 +1,23 @@
-// #include "Postman.hpp"
+#include "Postman.hpp"
 
-// class TCPPostman : public IPostman
-// {
-// public:
-//     TCPPostman(Args args);
-//     ~TCPPostman();
+class TCPPostman : public IPostman
+{
+public:
+    TCPPostman(Args args);
+    ~TCPPostman();
 
-//     void attach_to_server(const std::string &server_hostname, uint16_t port_number) override;
-//     int authorize(const std::string &username, const std::string &display_name, const std::string &password) override;
-//     int join(const std::string &channel_id, const std::string &display_name) override;
-//     int message(const std::string &display_name, const std::string &message_contents) override;
-//     int error(const std::string &display_name, const std::string &message_contents) override;
-//     int bye() override;
-//     int confirm() override;
-//     int get_client_socket() override;
-//     struct sockaddr_in get_server_address() override;
-//     PollResults poll_for_messages() override;
-
-//     Message receive() override;
-// };
+    std::string get_display_name() override;
+    void attach_to_server(const std::string &server_hostname, uint16_t port_number) override;
+    int authorize(const std::string &username, const std::string &display_name, const std::string &password) override;
+    int join(const std::string &channel_id, const std::string &display_name) override;
+    int message(const std::string &display_name, const std::string &message_contents) override;
+    int error(const std::string &display_name, const std::string &message_contents) override;
+    int bye() override;
+    int get_client_socket() override;
+    struct sockaddr_in get_server_address() override;
+    PollResults poll_for_messages() override;
+    Message receive() override;
+    void allow_client_commands(std::vector<CommandType> messages) override;
+    PollResults handle_server_message() override;
+    PollResults handle_user_command() override;
+};
--- src/TcpPostman.cpp
+++ src/TcpPostman.cpp
@@ -1,94 +1,175 @@
-// #include "TcpPostman.hpp"
+#include "TcpPostman.hpp"
 
-// TCPPostman::TCPPostman(Args args)
-// {
-//     // Create the socket
-//     client_socket = socket(AF_INET, SOCK_STREAM, 0);
-//     if (client_socket < 0)
-//     {
-//         throw std::runtime_error("ERROR opening socket");
-//     }
+TCPPostman::TCPPostman(Args args)
+{
+    // Create the socket
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket < 0)
+    {
+        throw std::runtime_error("ERROR opening socket");
+    }
 
-//     // Attach to the server
-//     this->attach_to_server(args.hostname, args.port);
-// }
+    // Attach to the server
+    this->attach_to_server(args.hostname, args.port);
 
-// TCPPostman::~TCPPostman()
-// {
-//     close(client_socket);
-// }
+    // Connect to the server
+    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        throw std::runtime_error("ERROR connecting to the TCP server");
+    }
+}
 
-// void TCPPostman::attach_to_server(const std::string &server_hostname, uint16_t port_number)
-// {
-//     // Set the server address
-//     std::memset(&server_address, 0, sizeof(server_address));
+TCPPostman::~TCPPostman()
+{
+    close(client_socket);
+}
 
-//     // Get the server by its hostname
-//     struct hostent *server = gethostbyname(server_hostname.c_str());
-//     if (server == nullptr)
-//     {
-//         throw std::runtime_error("ERROR, no such host as '" + server_hostname + "'");
-//     }
+std::string TCPPostman::get_display_name()
+{
+    return display_name;
+}
 
-//     // Set the server address
-//     server_address.sin_family = AF_INET;
-//     std::memcpy(&server_address.sin_addr.s_addr, server->h_addr, server->h_length);
-//     server_address.sin_port = htons(port_number);
+void TCPPostman::attach_to_server(const std::string &server_hostname, uint16_t port_number)
+{
+    // Set the server address
+    std::memset(&server_address, 0, sizeof(server_address));
 
-//     // Connect to the server
-//     if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
-//     {
-//         throw std::runtime_error("ERROR connecting to server");
-//     }
-// }
+    // Get the server by its hostname
+    struct hostent *server = gethostbyname(server_hostname.c_str());
+    if (server == nullptr)
+    {
+        throw std::runtime_error("ERROR, no such host as '" + server_hostname + "'");
+    }
 
-// // int TCPPostman::authorize(const std::string &username, const std::string &display_name, const std::string &password)
-// // {
-// //     // TODO: Implement this function
-// //     return 0;
-// // }
+    // Set the server address
+    server_address.sin_family = AF_INET;
+    std::memcpy(&server_address.sin_addr.s_addr, server->h_addr, server->h_length);
+    server_address.sin_port = htons(port_number);
+}
 
-// // int TCPPostman::join(const std::string &channel_id, const std::string &display_name)
-// // {
-// //     // TODO: Implement this function
-// //     return 0;
-// // }
+int TCPPostman::authorize(const std::string &username, const std::string &display_name, const std::string &password)
+{
+    // Assemble the message body
+    std::string message = "AUTH " + username + " AS " + display_name + " USING " + password + "\r\n";
 
-// // int TCPPostman::message(const std::string &display_name, const std::string &message_contents)
-// // {
-// //     // TODO: Implement this function
-// //     return 0;
-// // }
+    // Send the message
+    int n = send(client_socket, message.c_str(), message.length(), 0);
 
-// // int TCPPostman::error(const std::string &display_name, const std::string &message_contents)
-// // {
-// //     // TODO: Implement this function
-// //     return 0;
-// // }
+    // check for errors
+    if (n < 0)
+    {
+        throw std::runtime_error("ERROR sending AUTH message");
+    }
 
-// // int TCPPostman::bye()
-// // {
-// //     // TODO: Implement this function
-// //     return 0;
-// // }
+    last_sent_message = Message{};
+    last_sent_message.type = MessageType::AUTH;
+    last_sent_message.username = username;
+    last_sent_message.display_name = display_name;
+    last_sent_message.password = password;
 
-// // int TCPPostman::confirm()
-// // {
-// //     // TODO: Implement this function
-// //     return 0;
-// // }
+    // Disable user input after joining a channel until a reply is received
+    is_waiting_for_reply = true;
+    std::cout << FBLU("Waiting for reply...") << std::endl;
 
-// // int TCPPostman::get_client_socket()
-// // {
-// //     return client_socket;
-// // }
+    return 0;
+}
 
-// // struct sockaddr_in TCPPostman::get_server_address()
-// // {
-// //     return server_address;
-// // }
+int TCPPostman::join(const std::string &channel_id, const std::string &display_name)
+{
+    // Assemble the message body
+    std::string message = "JOIN " + channel_id + " AS " + display_name + "\r\n";
 
-// // PollResults TCPPostman::poll_for_messages()
-// // {
-// //     // TODO: Implement this function
-// // }
+    // Send the message
+    int n = send(client_socket, message.c_str(), message.length(), 0);
+
+    // check for errors
+    if (n < 0)
+    {
+        throw std::runtime_error("ERROR sending JOIN message");
+    }
+
+    last_sent_message = Message{};
+    last_sent_message.type = MessageType::JOIN;
+    last_sent_message.channel_id = channel_id;
+    last_sent_message.display_name = display_name;
+
+    // Disable user input after joining a channel until a reply is received
+    is_waiting_for_reply = true;
+    std::cout << FBLU("Waiting for reply...") << std::endl;
+
+    return 0;
+}
+
+int TCPPostman::message(const std::string &display_name, const std::string &message_contents)
+{
+    // Assemble the message body
+    std::string message = "MSG FROM " + display_name + " IS " + message_contents + "\r\n";
+
+    // Send the message
+    int n = send(client_socket, message.c_str(), message.length(), 0);
+
+    // check for errors
+    if (n < 0)
+    {
+        throw std::runtime_error("ERROR sending MSG message");
+    }
+
+    last_sent_message = Message{};
+    last_sent_message.type = MessageType::MSG;
+    last_sent_message.display_name = display_name;
+    last_sent_message.contents = message_contents;
+
+    return 0;
+}
+
+int TCPPostman::error(const std::string &display_name, const std::string &message_contents)
+{
+    // Assemble the message body
+    std::string message = "ERR FROM " + display_name + " IS " + message_contents + "\r\n";
+
+    // Send the message
+    int n = send(client_socket, message.c_str(), message.length(), 0);
+
+    // check for errors
+    if (n < 0)
+    {
+        throw std::runtime_error("ERROR sending MSG message");
+    }
+
+    last_sent_message = Message{};
+    last_sent_message.type = MessageType::ERR;
+    last_sent_message.display_name = display_name;
+    last_sent_message.contents = message_contents;
+
+    return 0;
+}
+
+int TCPPostman::bye()
+{
+    // Assemble the message body
+    std::string message = "BYE\r\n";
+
+    // Send the message
+    int n = send(client_socket, message.c_str(), message.length(), 0);
+
+    // check for errors
+    if (n < 0)
+    {
+        throw std::runtime_error("ERROR sending BYE message");
+    }
+
+    last_sent_message = Message{};
+    last_sent_message.type = MessageType::BYE;
+
+    return 0;
+}
+
+int TCPPostman::get_client_socket()
+{
+    return client_socket;
+}
+
+struct sockaddr_in TCPPostman::get_server_address()
+{
+    return server_address;
+}
